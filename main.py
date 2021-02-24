6ff4bda4c4c3d46d54c6ae5ef2a531e9e5a843a4
Вадим <popperony@yandex.ru>
2021-02-24 19:51:15 +0300

    по PEP8

--- main.py
+++ main.py
@@ -12,18 +12,36 @@ app = FastAPI()
 def index(query: str):
     response = parsing(query)
     try:
-        return {'Request': query, 'link_image_1': response[0][0], 'link_image_2': response[0][1], 'link_image_3': response[0][2], 'average_price': response[1]}
+        return {
+            'Request': query,
+            'link_image_1': response[0][0],
+            'link_image_2': response[0][1],
+            'link_image_3': response[0][2],
+            'average_price': response[1]
+            }
     except IndexError:
-        return {'Request': query,'Error': 'no result or parser was blocked'}
+        return {
+            'Request': query,
+            'Error': 'no result or parser was blocked'
+            }
 
 
 @app.get("/{query}/{discription}")
 def read_item(query: str, discription: str):
     response = parsing(discription)
     try:
-        return {'Request': discription, 'link_image_1': response[0][0], 'link_image_2': response[0][1], 'link_image_3': response[0][2], 'average_price': response[1]}
+        return {
+            'Request': discription,
+            'link_image_1': response[0][0],
+            'link_image_2': response[0][1],
+            'link_image_3': response[0][2],
+            'average_price': response[1]
+            }
     except IndexError:
-        return {'Request': discription,'Error': 'no result or parser was blocked'}
+        return {
+            'Request': discription,
+            'Error': 'no result or parser was blocked'
+            }
 
 @app.post("/query")
 def add_queries(item: Queries):
